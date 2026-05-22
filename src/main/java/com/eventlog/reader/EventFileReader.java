5dba2df71a9fed88edd6f82abe7123d7278e34e0
Kero <keremsunayhalil@gmail.com>
2026-05-22 20:29:38 +0300

    Add EventFileReader and EventParser

---
+++ src/main/java/com/eventlog/reader/EventFileReader.java
@@ -0,0 +1,13 @@
+package main.java.com.eventlog.reader;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class EventFileReader {
+
+    public List<String> readLines(String filePath) throws IOException {
+        return Files.readAllLines(Path.of(filePath));
+    }
+}
